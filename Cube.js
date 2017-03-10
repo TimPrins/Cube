94b6ac8e46a262fa8da587ea3e77ea8bb24af097
TimPrins <tim-prins@live.nl>
2017-03-10 17:13:13 +0100

    Cube.js now with comments

--- Cube.js
+++ Cube.js
@@ -8,6 +8,7 @@ class Cube {
 		this.rateOfGrowth = 1
 	}
 
+//Checks if the cube is bigger than 2, if the cube is bigger than 2 it's still alive!
 	isAlive() {
 		if(this.size > 2){
 			//console.log('The cube is still alive');
@@ -18,43 +19,51 @@ class Cube {
 		}
 	}
 
+//Returns the position of the cube	
 	getPosition() {
 		//console.log('The position of the cube is', this.position);
 		return this.position; 
 	}
 	
+//Returns the size of the cube
 	getSize() {
 		console.log('The size of the cube is', this.size);
 		return this.size;
 	}
 
+//Returns the volume of the cube
 	getVolume() {
 		let volume =  this.size * this.size * this.size;
 		//console.log('The volume of the cube is', volume);
 		return volume;
 	}
 
+//Set the position of the cube
 	setPosition(setPositionX, setPositionY) {
 		this.position.x = setPositionX
 		this.position.y = setPositionY
 	}
 
+//Set the new position of the cube
 	setVelocity(velocityX1, velocityY1) {
 		this.velocity.x = velocityX1
 		this.velocity.y = velocityY1
 		//console.log('The new velocity of the cube will be', ' x=', this.velocity.x, 'y=', this.velocity.y);
 	}
 
+//Set the rate of growth
 	setRateOfGrowth(rateOfGrowth) {
 		this.rateOfGrowth = rateOfGrowth;
 		//console.log('You have set the rate of growth of the cube to', rateOfGrowth)
 	}
 
+//Returns the rate of growth
 	getRateOfGrowth() {
 		//console.log('The rate of growth of the cube is', this.rateOfGrowth);
 		return this.rateOfGrowth;
 	}
 
+//Moves the whole cube 1 tick (1 second) forward in time
 	tick() {
 		//Variables needed to calculate where the cube is now
 		this.position.x = this.velocity.x + this.position.x
@@ -68,8 +77,9 @@ class Cube {
 }
 
 
+
 //new Cube(size)
-let Cube2 = new Cube(23);
+let Cube2 = new Cube(0);
 
 //Checks if the cube is bigger than 2 if is bigger than 2 it returns true
 Cube2.isAlive();
@@ -84,38 +94,17 @@ Cube2.getSize();
 Cube2.getVolume();
 	
 //Cube2.setVelocity(velocityX, velocityY)
-Cube2.setVelocity(8, 12);
+Cube2.setVelocity(0, 0);
 
 //Sets the new position of the cube
 Cube2.setPosition();
 
 //Sets the rate of the growth of the cube
 //Cube2.setRateOfGrowth(rateOfGrowth);
-Cube2.setRateOfGrowth(2);
+Cube2.setRateOfGrowth(0);
 
 //Prints and returns the rate of growth of the cube
 Cube2.getRateOfGrowth();
 
 //Makes the time in which the cube is go 1 tick (second) forward
 Cube2.tick()
-Cube2.getSize()
-Cube2.tick()
-Cube2.getSize()
-Cube2.tick()
-Cube2.getSize()
-Cube2.tick()
-Cube2.getSize()
-Cube2.tick()
-Cube2.getSize()
-Cube2.setRateOfGrowth(0,5);
-Cube2.tick()
-Cube2.getSize()
-Cube2.tick()
-Cube2.getSize()
-Cube2.tick()
-Cube2.getSize()
-Cube2.setRateOfGrowth(2);
-Cube2.tick()
-Cube2.getSize()
-Cube2.tick()
-Cube2.getSize()
